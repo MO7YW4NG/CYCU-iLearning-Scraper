882a8b25092f80312a68b05413986286d80a9c17
MO7YW4NG <motywang@gmail.com>
2024-10-02 04:16:55 +0800

    Added icon.png

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author="MO7YW4NG, Joe Liao",
     options={'build_exe': {
         'packages': ['aiohttp','json','getpass','os','hashlib','base64','re','aiodns','time','asyncio','Crypto','bs4','rich'],
-        'include_files': ['icon.ico']
+        'include_files': ['icon.ico','icon.png']
     }, 'bdist_msi': {'initial_target_dir': r'[DesktopFolder]\\CYCU-iLearning-Scraper'}},
     executables=[target],
 )
